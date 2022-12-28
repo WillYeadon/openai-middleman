302a2460b6e83e9a3399f526f43023bcfb413510
Will Yeadon <chvyalthan@gmail.com>
2022-12-28 22:51:59 +0000

    containers

---
+++ script.js
@@ -0,0 +1,76 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300)
+}
+
+function typeText(element, text) {
+  let index = 0;
+  let interval = setInterval(() => {
+    if (interval < text.length) {
+      element.innerHTML += text.chartAt(index)
+      index++;
+    } else {
+      clearInterval(interval)
+    }
+  }, 20)
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexaDecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexaDecimalString}`;
+}
+
+function chatStrip(isAi, value, uniqueId) {
+  return (
+    `
+      <div class="wrapper ${isAi && 'ai'}">
+        <div class="chat">
+          <div className="profile">
+            <img src="${isAi ? bot : user}"
+            alt="${isAi ? 'bot' : 'user'}"/>
+          </div>
+          <div class="message" id=${uniqueId}>${value}</div>
+        </div>
+      </div>
+    `
+  )
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // user chatStrip
+  chatContainer.innerHTML += chatStrip(false, data.get('prompt'));
+  form.reset();
+
+  // bot chatStrip
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStrip(true, ' ', uniqueId);
+  form.reset();
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId)
+
+  loader(messageDiv);
+} 
